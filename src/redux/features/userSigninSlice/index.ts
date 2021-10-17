d0f745bfc8debd50316653a363dd4f2235fa1beb
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-10-17 18:02:05 +0500

    !feat(CT-4/InputScreen): async thunk made

--- src/redux/features/userSigninSlice/index.ts
+++ src/redux/features/userSigninSlice/index.ts
@@ -1,54 +1,57 @@
-// import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-// import { Instance } from '../../../config/axios';
-// import { SERVER_IP } from '../../../config/constants';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { Instance } from '../../../config/axios';
+import { SERVER_IP } from '../../../config/constants';
 // import { userSignupSlice } from '../userSignupSlice';
 
-// export const loginUser = createAsyncThunk(('user-signin/loginUser'), async ({ name, password }:ILoginUserTypes) => {
-//   // try {
-//   //   const response = await Instance({
-//   //     method: 'POST',
-//   //     baseURL: `${SERVER_IP}/user/login`,
-//   //     headers: {
-//   //       Accept: 'application/json',
-//   //       'Content-Type': 'application/json'
-//   //     },
-//   //     data: { name, password }
-//   //   })
-//   //   const data = response;
-//   //   if (response.status === 200) {
-//   //     localStorage.setItem('User');
-//   //   }
-//   // } catch (err : any) {
-//   //   console.info('error')
-//   // } finally(err) {
-//   //   console.error("finally")
-//   // }
-// })
+export const loginUser = createAsyncThunk(('user-signin/loginUser'), async ({ name, password }:ILoginUserTypes, thunkAPI) => {
+  try {
+    const response = await Instance({
+      method: 'POST',
+      baseURL: `${SERVER_IP}/user/login`,
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      data: { name, password }
+    })
+    const data = await response;
+    if (response.status === 200) {
+      localStorage.setItem('token', JSON.stringify(data))
+      return { ...data }
+    }
+    return thunkAPI.rejectWithValue(data);
+    console.info(data)
+  } catch (e) {
+    console.error('error', e)
+    return thunkAPI.rejectWithValue(e)
+  }
+});
 
-// export interface ILoginUserTypes {
-//   name: string,
-//   password: string,
-//   loading?: boolean,
-//   success?: boolean,
-//   error?: boolean
-// }
-// export const initialState = {
-//   name: '',
-//   password: '',
-//   loading: false,
-//   success: false,
-//   error: false
-// }
+export interface ILoginUserTypes {
+  name: string,
+  password: string,
+  loading?: boolean,
+  success?: boolean,
+  error?: boolean,
+  message: string
+}
+export const initialState = {
+  name: '',
+  password: '',
+  loading: false,
+  success: false,
+  error: false
+}
 
-// export const userSigninSlice = createSlice({
-//   name: 'user-signin',
-//   initialState,
-//   reducers: {},
-//   extraReducers: {
+export const userSigninSlice = createSlice({
+  name: 'user-signin',
+  initialState,
+  reducers: {},
+  extraReducers: {
 
-//   }
-// })
+  }
+})
 
-// // export const {} = userSignupSlice.actions;
+// export const {} = userSignupSlice.actions;
 
 export default {}
