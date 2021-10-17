a43085920f6dbaef6b67d3cb4fef5335f74d4a29
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-10-17 14:10:31 +0500

    !fix: removed container rom button component's styles

--- src/components/button/index.styled.ts
+++ src/components/button/index.styled.ts
@@ -3,23 +3,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import 'antd/dist/antd.css'
 import Button from 'antd/es/button';
 
-export const Container = styled.div`
-  min-width: 64px;
-  height: 40px;
-  .ant-btn{
-    color: ${(props) => props.theme.color.white};
-    background: linear-gradient(270deg,#1A87BB, #1B9BD7);
-    border: none;
-    border-radius: 20px;
-    width: 100%;
-    font-size: 1rem;
-    height: 100%;
-    &:hover, &:focus, &:active {
-      color: ${(props) => props.theme.color.white};
-      background: linear-gradient(270deg,#1A87BB, #1B9BD7);
-    }
-  }
-`
 export const CustomButton = styled(Button)`
 `
 
