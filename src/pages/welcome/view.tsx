104b3154aaf78a654a084177172250260136b609
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-10-17 16:08:52 +0500

    !fix: fixes

--- src/pages/welcome/view.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react';
-import WelcomeComponent from '../../components/welcome';
-
-const ViewComponent: FC<{}> = () => (
-  <WelcomeComponent />
-)
-
-export default ViewComponent;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,10 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from '../pages/login';
-import Welcome from '../pages/welcome';
 
 const RouterComponent = () => (
   <div>
     <Router>
       <Switch>
-        <Route exact path="/" component={Welcome} />
         <Route exact path="/login" component={Login} />
       </Switch>
     </Router>
