a7a68a1847f42d345bba06bd0fbbb384861ff48b
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-10-19 11:42:08 +0500

    !hotfix: formatting improved

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,11 +1,25 @@
 import {
   ChangeEvent,
-  forwardRef, memo, MutableRefObject, useCallback, useRef, useState
-} from 'react';
-import { IconProp } from '@fortawesome/fontawesome-svg-core';
+  forwardRef,
+  memo,
+  MutableRefObject,
+  useCallback,
+  useRef,
+  useState
+} from 'react'
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
 // import debounce from 'lodash/debounce'
-import { faEye, faEyeSlash } from '@fortawesome/pro-light-svg-icons';
-import { InputContainer, InputField, Label, Prefix, Suffix } from './index.styled';
+import {
+  faEye,
+  faEyeSlash
+} from '@fortawesome/pro-light-svg-icons'
+import {
+  InputContainer,
+  InputField,
+  Label,
+  Prefix,
+  Suffix
+} from './index.styled'
 
 interface InputProps {
   label?: string
@@ -18,69 +32,85 @@ interface InputProps {
   handleChange?: (e: ChangeEvent<HTMLInputElement>) => void
 }
 
-const Input = memo(forwardRef<HTMLInputElement, InputProps>(({
-  label, value = '', name, handleChange, typePassword = false, prefix, suffix, ...props
-}, inputRef) => {
-  const localRef = useRef<HTMLInputElement>(null);
-  const [showPassword, setShowPassword] = useState(false);
-  // const [_value, setValue] = useState<typeof value>(value);
-
-  // useEffect(() => {
-  //   // setValue(value)
-  // }, [value])
-
-  // add debounce here later
-  // const handleInputDebounce = (e: ChangeEvent<HTMLInputElement>) => {
-  //   if (handleChange) handleChange(e);
-  // }
-
-  // const handleInputChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-  //   setValue(e.target.value)
-  //   handleInputDebounce(e);
-  // }, [_value])
-
-  const handleEyeChange = useCallback(() => {
-    setShowPassword((prevState) => !prevState)
-  }, [showPassword])
-
-  return (
-    <InputContainer>
-      <InputField
-        {...props}
-        name={name}
-        value={value}
-        type={typePassword && !showPassword ? 'password' : 'text'}
-        ref={(_ref) => {
-          if (_ref) (localRef as MutableRefObject<HTMLInputElement>).current = _ref;
-          if (inputRef) {
-            if (typeof inputRef === 'function') inputRef(_ref)
-            if (typeof inputRef === 'object') {
-              // eslint-disable-next-line no-param-reassign
-              inputRef.current = _ref;
-            }
-          }
-        }}
-        onChange={handleChange}
-      />
-      {prefix && (
-        <Prefix icon={prefix} />
-      )}
-      <Label hasValue={!!value}>
-        {label}
-      </Label>
-      {typePassword && (
-        <Suffix
-          onClick={handleEyeChange}
-          icon={!showPassword ? faEye : faEyeSlash}
-        />
-      )}
+const Input = memo(
+  forwardRef<HTMLInputElement, InputProps>(
+    (
       {
-        !typePassword && suffix && (
-          <Suffix icon={suffix} />
-        )
-      }
-    </InputContainer>
-  )
-}));
+        label,
+        value = '',
+        name,
+        handleChange,
+        typePassword = false,
+        prefix,
+        suffix,
+        ...props
+      },
+      inputRef
+    ) => {
+      const localRef = useRef<HTMLInputElement>(null)
+      const [showPassword, setShowPassword] = useState(false)
+      // const [_value, setValue] = useState<typeof value>(value);
 
-export default Input;
+      // useEffect(() => {
+      //   // setValue(value)
+      // }, [value])
+
+      // add debounce here later
+      // const handleInputDebounce = (e: ChangeEvent<HTMLInputElement>) => {
+      //   if (handleChange) handleChange(e);
+      // }
+
+      // const handleInputChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+      //   setValue(e.target.value)
+      //   handleInputDebounce(e);
+      // }, [_value])
+
+      const handleEyeChange = useCallback(() => {
+        setShowPassword((prevState) => !prevState)
+      }, [showPassword])
+
+      return (
+        <InputContainer>
+          <InputField
+            {...props}
+            name={name}
+            value={value}
+            type={
+              typePassword && !showPassword
+                ? 'password'
+                : 'text'
+            }
+            ref={(_ref) => {
+              if (_ref) {
+                (
+                  localRef as MutableRefObject<HTMLInputElement>
+                ).current = _ref
+              }
+              if (inputRef) {
+                if (typeof inputRef === 'function') { inputRef(_ref) }
+                if (typeof inputRef === 'object') {
+                  // eslint-disable-next-line no-param-reassign
+                  inputRef.current = _ref
+                }
+              }
+            }}
+            onChange={handleChange}
+          />
+          {prefix && <Prefix icon={prefix} />}
+          <Label hasValue={!!value}>{label}</Label>
+          {typePassword && (
+            <Suffix
+              onClick={handleEyeChange}
+              icon={!showPassword ? faEye : faEyeSlash}
+            />
+          )}
+          {!typePassword && suffix && (
+            <Suffix icon={suffix} />
+          )}
+        </InputContainer>
+      )
+    }
+  )
+)
+
+export default Input
