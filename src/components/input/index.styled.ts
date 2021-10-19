ad9f163fd717668685e3e2dc9c5657e88d8d2561
azam pasha <developer5.grafdom@gmail.com>
2021-10-19 12:46:05 +0500

    !(CT-10): padding added

--- src/components/input/index.styled.ts
+++ src/components/input/index.styled.ts
@@ -1,19 +1,23 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import styled from 'styled-components';
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable operator-linebreak */
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import styled from 'styled-components'
 
-export const Label = styled.p<{hasValue : boolean}>`
+export const Label = styled.p<{ hasValue: boolean }>`
   font-size: 1rem;
   position: absolute;
   opacity: 0.6;
   left: 3rem;
   pointer-events: none;
-  transition: all .22s;
+  transition: all 0.22s;
   top: 50%;
   transform: translateY(-50%);
   display: block;
   transition: top 200ms ease-in, left 200ms ease-in, font-size 200ms ease-in;
-
-   ${({ hasValue }) => hasValue && `
+  padding: 0 7px;
+  ${({ hasValue }) =>
+    hasValue &&
+    `
       top: 0;
       left: 0.8rem;
       font-size: 0.8rem;
@@ -29,13 +33,15 @@ export const InputField = styled.input`
   padding: 12px;
   background-color: transparent;
   padding-left: 3rem;
-  :focus ~ ${Label}{
+  :focus ~ ${Label} {
     top: 0;
     left: 0.8rem;
     font-size: 0.8rem;
-    background: linear-gradient(179deg, #F2F4FF 52%, white 48%);
+    background: linear-gradient(179deg, #f2f4ff 52%, white 48%);
+    opacity: 1;
+    color: ${(props) => props.theme.color.lightGray};
   }
-`;
+`
 
 export const InputContainer = styled.div`
   height: 100%;
@@ -46,11 +52,15 @@ export const InputContainer = styled.div`
   border-radius: 8px;
   z-index: 1;
   transition: 0.4s;
-
-  &:focus, &:hover{
+  &:focus,
+  &:hover {
     border: ${(props) => props.theme.borderColor.black};
   }
-`;
+  &:hover ~ ${Label} {
+    opacity: 1;
+    color: ${(props) => props.theme.color.lightGray};
+  }
+`
 
 export const Prefix = styled(FontAwesomeIcon)`
   position: absolute;
