7a15a0000bc52aa41d8f226fe9ba5397470144b0
azam pasha <developer5.grafdom@gmail.com>
2021-10-20 18:28:34 +0500

    QA fixes

--- src/components/genericButton/index.tsx
+++ src/components/genericButton/index.tsx
@@ -6,14 +6,15 @@ interface IButtonProps {
   label: string
   icon: IconProp
   type?: 'button' | 'submit' | 'reset' | undefined
+  // eslint-disable-next-line react/no-unused-prop-types
   color?: string
   // eslint-disable-next-line no-unused-vars
   handleSubmit?: (e: MouseEvent<HTMLButtonElement>) => void
 }
 
 const Button: FC<IButtonProps> = memo(
-  ({ label, icon, type, color, handleSubmit }: IButtonProps) => (
-    <CustomButton htmlType={type} color={color} onClick={handleSubmit}>
+  ({ label, icon, type, handleSubmit }: IButtonProps) => (
+    <CustomButton htmlType={type} onClick={handleSubmit}>
       <IconStyler icon={icon} />
       {label}
     </CustomButton>
