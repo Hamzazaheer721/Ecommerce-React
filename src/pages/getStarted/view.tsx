1470c1b85f259326bffca814c27ff7a8ebca01e1
azam pasha <developer5.grafdom@gmail.com>
2021-10-20 17:57:12 +0500

    modal completed

--- src/pages/getStarted/view.tsx
+++ src/pages/getStarted/view.tsx
@@ -1,11 +1,10 @@
 /* eslint-disable no-undef */
-import { FC, memo, useState } from 'react'
+import { FC, memo } from 'react'
 import {
   faUser,
   faBuilding
 } from '@fortawesome/pro-light-svg-icons'
 import { Link } from 'react-router-dom'
-import CustomizeModal from '../../components/modal'
 
 import {
   LoginHeader,
@@ -19,61 +18,38 @@ import {
   SecondaryButtonContainer
 } from '../../components/genericButton/index.styled'
 
-const ViewComponent: FC = memo(() => {
-  // eslint-disable-next-line operator-linebreak
-  const [isModalVisible, setIsModalVisible] =
-    useState(false)
-
-  const showModal = () => {
-    setIsModalVisible(true)
-  }
-
-  const closeModal: () => void = () => {
-    setIsModalVisible(false)
-  }
-
-  return (
-    <LoginViewContainer>
-      <LoginHeader>Get Started Free</LoginHeader>
-      <LoginDescription>
-        Its time to enjoy ultimate shoping experience
-        through WhatsApp
-      </LoginDescription>
-      <Link to="/register/customer">
-        <ButtonContainer>
-          <CustomButton
-            label="Customer"
-            icon={faUser}
-            type="button"
-            color="red"
-          />
-        </ButtonContainer>
-      </Link>
-      <Link to="/register/company">
-        <SecondaryButtonContainer>
-          <CustomButton
-            label="Company"
-            icon={faBuilding}
-            type="button"
-            color="red"
-          />
-        </SecondaryButtonContainer>
-      </Link>
-      <button type="button" onClick={showModal}>
-        open modal
-      </button>
-      {isModalVisible && (
-        <CustomizeModal
-          openModal={isModalVisible}
-          closeModal={closeModal}
+const ViewComponent: FC = memo(() => (
+  <LoginViewContainer>
+    <LoginHeader>Get Started Free</LoginHeader>
+    <LoginDescription>
+      Its time to enjoy ultimate shoping experience through
+      WhatsApp
+    </LoginDescription>
+    <Link to="/register/customer">
+      <ButtonContainer>
+        <CustomButton
+          label="Customer"
+          icon={faUser}
+          type="button"
+          color="red"
         />
-      )}
-      <LoginFooter>
-        Already on WhatsPays?
-        <span> Login</span>
-      </LoginFooter>
-    </LoginViewContainer>
-  )
-})
+      </ButtonContainer>
+    </Link>
+    <Link to="/register/company">
+      <SecondaryButtonContainer>
+        <CustomButton
+          label="Company"
+          icon={faBuilding}
+          type="button"
+          color="red"
+        />
+      </SecondaryButtonContainer>
+    </Link>
+    <LoginFooter>
+      Already on WhatsPays?
+      <span> Login</span>
+    </LoginFooter>
+  </LoginViewContainer>
+))
 
 export default ViewComponent
