19132bb228240f378aa7c364a3015ca7e19c95a0
azam pasha <developer5.grafdom@gmail.com>
2021-10-24 18:52:02 +0500

    fix(CT-13/homePage): deals helper added

--- src/components/home/components/deals/index.tsx
+++ src/components/home/components/deals/index.tsx
@@ -2,31 +2,12 @@ import { memo } from 'react'
 import Carousel from 'react-multi-carousel'
 import 'react-multi-carousel/lib/styles.css'
 import { Container, DealImage } from './index.styled'
-
-const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 6
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 6
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 6
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 3
-  }
-}
+import { Responsive } from './helper'
 
 const Deals = memo(() => (
   <>
     <Container>
-      <Carousel responsive={responsive}>
+      <Carousel responsive={Responsive}>
         <DealImage src="/images/test-5.jpg" />
         <DealImage src="/images/test-5.jpg" />
         <DealImage src="/images/test-5.jpg" />
