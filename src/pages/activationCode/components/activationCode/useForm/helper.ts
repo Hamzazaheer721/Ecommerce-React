d1b209bea9167e1e6faf0d1fd02b6d40ef3f7656
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-10-25 15:04:09 +0500

    !feat(CT-14/activation): added errors and other states for activation form

---
+++ src/pages/activationCode/components/activationCode/useForm/helper.ts
@@ -0,0 +1,6 @@
+import { IActivationType } from './types';
+
+export const initialState : IActivationType = {
+  email: '',
+  activation_code: ''
+}
--- src/pages/activationCode/components/activationCode/useForm/index.tsx
+++ src/pages/activationCode/components/activationCode/useForm/index.tsx
@@ -1,18 +1,30 @@
-import { useMemo, useEffect } from 'react'
+import { useMemo, useEffect, useCallback, useState, ChangeEvent } from 'react'
 import { useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 import { RootState } from '../../../../../redux/store'
+import { initialState } from './helper'
+import { IActivationErrorType, IActivationType } from './types'
 
 const useForm = () => {
+  const history = useHistory()
+
+  const [activationData] = useState<IActivationType>(initialState)
+
+  const [error] = useState<Partial<IActivationErrorType>>({})
   const registerState = useSelector((state: RootState) => state.registerUser)
   const email = useMemo(() => registerState.user.email, [registerState])
-  const history = useHistory()
 
   useEffect(() => {
     !registerState.user.email && history.goBack()
   }, [])
 
-  return { email }
+  const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    console.info(e)
+  }, [])
+
+  const handleSubmit = useCallback(() => {}, [])
+
+  return { email, handleSubmit, handleChange, error, activationData }
 }
 
 export default useForm
