fb60b939b52e49d63aa4084a96c68a6031ad322b
azam pasha <developer5.grafdom@gmail.com>
2021-10-26 11:58:06 +0500

    fix:CT-14/toaster resend link added

--- src/components/toaster/index.tsx
+++ src/components/toaster/index.tsx
@@ -12,72 +12,82 @@ import {
 } from './index.styled'
 
 interface IToastProps {
-  toastType: 'success' | 'error' | 'info',
+  toastType: 'success' | 'error' | 'info'
   description?: string
+  resendLink?: boolean
 }
 
-const ToastComponent: FC<IToastProps> = memo(({ toastType, description }: IToastProps) => {
-  const colors: IColorObjectProp = useMemo(() => filterColor(toastType), [toastType])
-  return (
-    <ToastContainer colors={colors}>
-      <h2>
-        {toastType === 'success' && (
-          <CheckIcon icon={faCheck} colors={colors} />
-        )}
-        {toastType === 'error' && (
-          <CheckIcon icon={faTimes} colors={colors} />
-        )}
-        {toastType === 'info' && (
-          <CheckIcon icon={faInfo} colors={colors} />
-        )}
-      </h2>
-      <TitleContainer colors={colors}>
-        {toastType === 'success' && (
-          <h2>
-            Congratulation
-            <CrossIcon icon={faTimes} />
-          </h2>
-        )}
-        {toastType === 'error' && (
-          <h2>
-            Error
-            <CrossIcon icon={faTimes} />
-          </h2>
-        )}
-        {toastType === 'info' && (
-          <h2>
-            Info
-            <CrossIcon icon={faTimes} />
-          </h2>
-        )}
+const ToastComponent: FC<IToastProps> = memo(
+  ({ toastType, description, resendLink }: IToastProps) => {
+    const colors: IColorObjectProp = useMemo(
+      () => filterColor(toastType),
+      [toastType]
+    )
+    return (
+      <ToastContainer colors={colors}>
+        <h2>
+          {toastType === 'success' && (
+            <CheckIcon icon={faCheck} colors={colors} />
+          )}
+          {toastType === 'error' && (
+            <CheckIcon icon={faTimes} colors={colors} />
+          )}
+          {toastType === 'info' && <CheckIcon icon={faInfo} colors={colors} />}
+        </h2>
+        <TitleContainer colors={colors}>
+          {resendLink === true && (
+            <h2>
+              link here
+              <CrossIcon icon={faTimes} />
+            </h2>
+          )}
+          {toastType === 'success' && (
+            <h2>
+              Congratulation
+              <CrossIcon icon={faTimes} />
+            </h2>
+          )}
+          {toastType === 'error' && (
+            <h2>
+              Error
+              <CrossIcon icon={faTimes} />
+            </h2>
+          )}
+          {toastType === 'info' && (
+            <h2>
+              Info
+              <CrossIcon icon={faTimes} />
+            </h2>
+          )}
 
-        {toastType === 'success' && (
-          <Description>
-            {description}
-            {!description && <span>Your Account has been successfully activated!</span>}
-          </Description>
-        )}
-        {toastType === 'error' && (
-          <Description>
-            {description}
-            {!description && (
-              <>
-                <span>Password </span>
-                {' '}
-                doesn`t match!
-                {' '}
-              </>
-            )}
-          </Description>
-        )}
-        {toastType === 'info' && (
-          <Description>
-            {description}
-            {!description && <>Enter your New Password!</>}
-          </Description>
-        )}
-      </TitleContainer>
-    </ToastContainer>
-  )
-})
+          {toastType === 'success' && (
+            <Description>
+              {description}
+              {!description && (
+                <span>Your Account has been successfully activated!</span>
+              )}
+            </Description>
+          )}
+          {toastType === 'error' && (
+            <Description>
+              {description}
+              {!description && (
+                <>
+                  <span>Password </span>
+                  doesn`t match!
+                </>
+              )}
+            </Description>
+          )}
+          {toastType === 'info' && (
+            <Description>
+              {description}
+              {!description && <>Enter your New Password!</>}
+            </Description>
+          )}
+        </TitleContainer>
+      </ToastContainer>
+    )
+  }
+)
 export default ToastComponent
