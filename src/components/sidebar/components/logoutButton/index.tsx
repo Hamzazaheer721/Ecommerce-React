9f385c3bd0a2401da1b2ee69d5adf1a58c25118d
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-10-27 19:16:10 +0500

    !hotfix: added navigation for logout button

--- src/components/sidebar/components/logoutButton/index.tsx
+++ src/components/sidebar/components/logoutButton/index.tsx
@@ -1,16 +1,20 @@
 import { memo } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSignOut } from '@fortawesome/pro-light-svg-icons'
+import { useHistory } from 'react-router-dom'
 import { LogoutButton, WrapContainer } from './index.styled'
 
-const LogoutButtonComponent = memo(() => (
-  <>
-    <WrapContainer>
-      <LogoutButton>
-        <FontAwesomeIcon icon={faSignOut} style={{ marginRight: '4px' }} />
-        Logout
-      </LogoutButton>
-    </WrapContainer>
-  </>
-))
+const LogoutButtonComponent = memo(() => {
+  const history = useHistory()
+  return (
+    <>
+      <WrapContainer>
+        <LogoutButton onClick={() => history.push('/login')}>
+          <FontAwesomeIcon icon={faSignOut} style={{ marginRight: '4px' }} />
+          Logout
+        </LogoutButton>
+      </WrapContainer>
+    </>
+  )
+})
 export default LogoutButtonComponent
