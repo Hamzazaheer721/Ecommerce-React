4020ca1baa2b637464656fa1919e0102583b00e2
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-10-27 18:03:59 +0500

    !(featCT-16/forgotPassword): slice reducers and extra reducers have been made

--- src/redux/features/forgotPasswordSlice/index.ts
+++ src/redux/features/forgotPasswordSlice/index.ts
@@ -1,9 +1,33 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { initialState } from './helper';
+import { createSlice } from '@reduxjs/toolkit'
+import { forgotPassword } from './apiAction'
+import { initialState } from './helper'
 
 export const forgotPasswordSlice = createSlice({
   name: 'forgot',
   initialState,
-  reducers: {},
-  extraReducers: {}
+  reducers: {
+    clearForgotPaswordSliceAllStates: (state) => {
+      state.loading = false
+      state.message = ''
+      state.success = false
+    }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(forgotPassword.fulfilled, (state, { payload }) => {
+        state.loading = false
+        state.message = payload.message
+        state.success = payload.success
+      })
+      .addCase(forgotPassword.pending, (state) => {
+        state.loading = true
+      })
+      .addCase(forgotPassword.rejected, (state, { payload }) => {
+        state.loading = false
+        state.message = payload!.message
+        state.success = payload!.success
+      })
+  }
 })
+
+export const { clearForgotPaswordSliceAllStates } = forgotPasswordSlice.actions
