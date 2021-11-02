49107be497069fe5690cc2a35e4375ebacd1eb9b
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-11-02 15:38:07 +0500

    !feat(CT-18/profile): added store profile page

--- src/pages/store/components/fieldsSection/index.styled.ts
+++ src/pages/store/components/fieldsSection/index.styled.ts
@@ -1,6 +1,32 @@
 import styled from 'styled-components';
+import { device } from '../../../../styles/devices';
 
 export const FieldsSectionContainer = styled.div`
   flex: 1 1 75%;
   background-color: bisque;
+  display: flex;
+  flex-direction: column;
+  padding: 10px 20px 0 20px;
+  gap: 20px;
+`
+
+export const InputsContainer = styled.div`
+  display: flex;
+  width: 100%;
+  gap: 20px;
+  flex-wrap: wrap;
+`
+export const SingleInputContainer = styled.div`
+  border:none;
+  outline:none;
+  width: 100%;
+`
+export const InputContainer = styled.div<{single? : boolean}>`
+  border: none;
+  outline: none;
+  flex: 1 1 40%;
+  @media ${device.mobile}{
+    flex: 1 1 100%;
+  }
+  
 `
--- src/pages/store/components/fieldsSection/index.tsx
+++ src/pages/store/components/fieldsSection/index.tsx
@@ -1,8 +1,49 @@
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+import { faUser } from '@fortawesome/pro-light-svg-icons'
 import { FC, memo } from 'react'
-import { FieldsSectionContainer } from './index.styled'
+import Input from '../../../../components/input'
+import {
+  FieldsSectionContainer,
+  InputContainer,
+  InputsContainer,
+  SingleInputContainer
+} from './index.styled'
 
-const FieldsSection: FC = memo(() => (
-  <FieldsSectionContainer>fields Section</FieldsSectionContainer>
-))
+const FieldsSection: FC = memo(() => {
+  console.log('fieldsSection')
+  return (
+    <FieldsSectionContainer>
+      <SingleInputContainer>
+        <Input prefix={faUser} label="Bussiness Profile" />
+      </SingleInputContainer>
+
+      <InputsContainer>
+        <InputContainer>
+          <Input
+            label="WhatsApp Number"
+            phonefield
+            prefix={faWhatsapp}
+            store
+            // value={phone}
+            // handlePhoneChange={handlePhoneChange}
+          />
+        </InputContainer>
+
+        <InputContainer>
+          <Input prefix={faUser} label="Bussiness Profile" />
+        </InputContainer>
+      </InputsContainer>
+
+      <InputsContainer>
+        <InputContainer>
+          <Input />
+        </InputContainer>
+        <InputContainer>
+          <Input />
+        </InputContainer>
+      </InputsContainer>
+    </FieldsSectionContainer>
+  )
+})
 
 export default FieldsSection
