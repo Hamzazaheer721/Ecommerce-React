9b676622ef4cf6627296d7fc04fe7cfdcaf08bbb
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-11-03 11:56:46 +0500

    !hotfix: removed profile

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,7 +9,6 @@ import ActivationCode from '../pages/activationCode'
 import ForgotPassword from '../pages/forgotPassword'
 import ResetPassword from '../pages/resetPassword'
 import CustomizeModal from '../components/modal'
-import UserProfile from '../pages/userProfile/userProfile'
 import Store from '../pages/store'
 
 const RouterComponent = memo(() => (
@@ -25,7 +24,6 @@ const RouterComponent = memo(() => (
       <Route exact path="/forgot-password" component={ForgotPassword} />
       <Route exact path="/reset-password" component={ResetPassword} />
       <Route exact path="/reset-password/:userId" component={ResetPassword} />
-      <Route exact path="/user-profile" component={UserProfile} />
       <Route exact path="/store" component={Store} />
     </Switch>
   </Router>
