fbe92383aef17cfac7185e9a0eab16e06d349525
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-11-03 11:57:09 +0500

    !hotfix: removed profile

--- src/pages/userProfile/profileImage/index.styled.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const Container = styled.div`
-  text-align: center;
-`
-export const UserProfileImage = styled.img`
-  min-height: 200px;
-  min-width: 200px;
-`
--- src/pages/userProfile/view.tsx
+++
@@ -1,6 +0,0 @@
-import { FC, memo } from 'react'
-import UserProfileComponent from './userProfile'
-
-const ViewComponent: FC = memo(() => <UserProfileComponent />)
-
-export default ViewComponent
