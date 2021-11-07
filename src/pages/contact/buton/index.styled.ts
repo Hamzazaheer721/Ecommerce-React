d9677d24d3b01727a7e99beef69882b1f4db7ea9
azam pasha <developer5.grafdom@gmail.com>
2021-11-07 19:15:02 +0500

    contact done

---
+++ src/pages/contact/buton/index.styled.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+import { device } from '../../../styles/devices'
+
+export const ButtonContainerInner = styled.div`
+  height: 40px;
+  width: 50%;
+  padding: 0 20px;
+  margin: auto;
+  @media ${device.mobile} {
+    width: 100%;
+  }
+  .ant-btn {
+    color: ${(props) => props.theme.color.white};
+    background: linear-gradient(270deg, #1a87bb, #1b9bd7);
+    border: none;
+    border-radius: 20px;
+    width: 100%;
+    font-size: 1rem;
+    height: 100%;
+    &:hover,
+    &:focus,
+    &:active {
+      color: ${(props) => props.theme.color.white};
+      background: linear-gradient(270deg, #1a87bb, #1b9bd7);
+    }
+  }
+`
