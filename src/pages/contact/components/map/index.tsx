63aa02e4dfa27be402afe159583e7035d3f66d34
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-11-08 12:38:16 +0500

    !fix(CT-24/map): stopped the map propogation

--- src/pages/contact/components/map/index.tsx
+++ src/pages/contact/components/map/index.tsx
@@ -1,6 +1,11 @@
 import { memo } from 'react'
 import MapComponent from '../../../../components/map'
+import { MapContainer } from '../../../../styles/global'
 
-const Map = memo(() => <MapComponent />)
+const Map = memo(() => (
+  <MapContainer height="300px">
+    <MapComponent />
+  </MapContainer>
+))
 
 export default Map
