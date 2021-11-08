ae23a9c79b59f9ba864aba2c763420ad43b04f3d
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-11-08 13:03:22 +0500

    !hotfix: added redux into inputfields component

--- src/pages/contact/components/inputFields/index.tsx
+++ src/pages/contact/components/inputFields/index.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react'
+import { memo, useEffect } from 'react'
 import {
   faBuilding,
   faTimesCircle,
@@ -15,6 +15,7 @@ import {
   faBrowser,
   faLocation
 } from '@fortawesome/pro-light-svg-icons'
+import { useSelector } from 'react-redux'
 import {
   FieldsSectionContainer,
   SingleInputContainer,
@@ -26,59 +27,73 @@ import {
 } from './index.styled'
 
 import Input from '../../../../components/input'
+import { RootState } from '../../../../redux/store'
 
-const LocationInputFields = memo(() => (
-  <FieldsSectionContainer>
-    <SingleInputContainer>
-      <Input
-        label="Location"
-        prefix={faMapMarkerAlt}
-        suffix={faTimesCircle}
-        secondSuffix={faLocation}
-      />
-    </SingleInputContainer>
-    <SingleInputContainer>
-      <Input label="Street Address" prefix={faHome} store />
-    </SingleInputContainer>
-    <InputsContainer>
-      <InputContainer>
-        <Input label="Postal Code" prefix={faMailbox} />
-      </InputContainer>
-      <InputContainer>
-        <Input prefix={faChartArea} label="Area" />
-      </InputContainer>
-    </InputsContainer>
-    <InputsContainer>
-      <InputContainer>
-        <Input prefix={faBuilding} label="City" />
-      </InputContainer>
-      <InputContainer>
-        <Input prefix={faCity} label="State" />
-      </InputContainer>
-    </InputsContainer>
-    <SingleInputContainer>
-      <Input label="Country" prefix={faGlobe} />
-    </SingleInputContainer>
-    <CheckboxContainer>
-      <CustomCheckBox type="checkbox" name="remember" />
-      <CheckboxPlaceholder>Online Store only</CheckboxPlaceholder>
-    </CheckboxContainer>
-    <SingleInputContainer>
-      <Input label="Phone Number" prefix={faPhoneAlt} phonefield store />
-    </SingleInputContainer>
-    <SingleInputContainer>
-      <Input label="Mobile Number" prefix={faMobileAndroid} phonefield store />
-    </SingleInputContainer>
-    <SingleInputContainer>
-      <Input label="Email" prefix={faEnvelope} />
-    </SingleInputContainer>
-    <SingleInputContainer>
-      <Input label="Website" prefix={faBrowser} />
-    </SingleInputContainer>
-    <SingleInputContainer>
-      <Input label="NTN Number" prefix={faClipboardList} />
-    </SingleInputContainer>
-  </FieldsSectionContainer>
-))
+const LocationInputFields = memo(() => {
+  const { lat, long } = useSelector(
+    (state: RootState) => state.currentGeoLocation
+  )
+
+  useEffect(() => {}, [lat, long])
+
+  return (
+    <FieldsSectionContainer>
+      <SingleInputContainer>
+        <Input
+          label="Location"
+          prefix={faMapMarkerAlt}
+          suffix={faTimesCircle}
+          secondSuffix={faLocation}
+        />
+      </SingleInputContainer>
+      <SingleInputContainer>
+        <Input label="Street Address" prefix={faHome} store />
+      </SingleInputContainer>
+      <InputsContainer>
+        <InputContainer>
+          <Input label="Postal Code" prefix={faMailbox} />
+        </InputContainer>
+        <InputContainer>
+          <Input prefix={faChartArea} label="Area" />
+        </InputContainer>
+      </InputsContainer>
+      <InputsContainer>
+        <InputContainer>
+          <Input prefix={faBuilding} label="City" />
+        </InputContainer>
+        <InputContainer>
+          <Input prefix={faCity} label="State" />
+        </InputContainer>
+      </InputsContainer>
+      <SingleInputContainer>
+        <Input label="Country" prefix={faGlobe} />
+      </SingleInputContainer>
+      <CheckboxContainer>
+        <CustomCheckBox type="checkbox" name="remember" />
+        <CheckboxPlaceholder>Online Store only</CheckboxPlaceholder>
+      </CheckboxContainer>
+      <SingleInputContainer>
+        <Input label="Phone Number" prefix={faPhoneAlt} phonefield store />
+      </SingleInputContainer>
+      <SingleInputContainer>
+        <Input
+          label="Mobile Number"
+          prefix={faMobileAndroid}
+          phonefield
+          store
+        />
+      </SingleInputContainer>
+      <SingleInputContainer>
+        <Input label="Email" prefix={faEnvelope} />
+      </SingleInputContainer>
+      <SingleInputContainer>
+        <Input label="Website" prefix={faBrowser} />
+      </SingleInputContainer>
+      <SingleInputContainer>
+        <Input label="NTN Number" prefix={faClipboardList} />
+      </SingleInputContainer>
+    </FieldsSectionContainer>
+  )
+})
 
 export default LocationInputFields
