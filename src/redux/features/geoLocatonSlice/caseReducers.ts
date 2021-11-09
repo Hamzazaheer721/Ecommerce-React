61af8ab6ea9a40cd3a31cc3ee6f9dfde73916c95
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-11-09 15:18:27 +0500

    !feat(CT-24/map): webworker has been implemented

--- src/redux/features/geoLocatonSlice/caseReducers.ts
+++ src/redux/features/geoLocatonSlice/caseReducers.ts
@@ -1,25 +1,19 @@
 import { CaseReducer, PayloadAction } from '@reduxjs/toolkit'
 import { IGeoLocationLongLat, IGeoLocationPayloadArg } from '../../../types/geoLocation'
 
-export const getArea = () => {
-
-}
-
-export const getState = () => {
-
-}
-
-export const getCity = () => {
-
-}
-
 export const updateLocationCaseReducer: CaseReducer<
   Partial<IGeoLocationLongLat>,
   PayloadAction<Partial<IGeoLocationPayloadArg>>
 > = (state, action) => {
   const { payload } = action
-  if (payload.geoCodeAddress) {
-    const locationWorker: Worker = new Worker('./workers/locationWorker.js')
-    locationWorker.postMessage(payload.geoCodeAddress)
+  if (payload.geoCodeAddress && state.address) {
+    const {geoCodeAddress} = payload;
+    state.address.area = geoCodeAddress?.area;
+    state.address.city = geoCodeAddress?.city;
+    state.address.country = geoCodeAddress?.country;
+    state.address.postalCode = geoCodeAddress?.postalCode;
+    state.address.state = geoCodeAddress?.state;
+    state.address.streetAddress = geoCodeAddress?.streetAddress;
+    state.address.location = geoCodeAddress?.location;
   }
 }
