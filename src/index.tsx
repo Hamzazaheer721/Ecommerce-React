8950f4d01bfe5d9455807def1e3fc91fa5954c2c
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-11-09 16:08:14 +0500

    !hotfix: netlify fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import persistStore from 'redux-persist/es/persistStore'
 import { PersistGate } from 'redux-persist/integration/react'
-
 import App from './App'
 import { store } from './redux/store'
 
