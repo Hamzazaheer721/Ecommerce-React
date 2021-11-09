985fc20258c410574a4552df0007fa5576e584e7
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-11-09 10:21:15 +0500

    !hotfix: refactored header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,28 +1,53 @@
-import { memo } from 'react'
+import { memo, useCallback, useContext, useMemo } from 'react'
+import { useHistory } from 'react-router-dom'
+import { faArrowLeft } from '@fortawesome/pro-light-svg-icons'
+import { ThemeContext } from 'styled-components'
+import Icon from '../icon'
+import SidebarComponent from '../sidebar'
 import InputComponent from './components/inputField'
+import HeaderRightComponent from './components/headerRightArea'
 import {
   HeaderContainer,
   HeaderIconContainer,
   HeaderInputContainer,
   HeaderRightIconsContainer
 } from './index.styled'
-import SidebarComponent from '../sidebar'
-import HeaderRightComponent from './components/headerRightArea'
 
-const HeaderComponent = memo(() => (
-  <>
-    <HeaderContainer>
-      <HeaderIconContainer>
-        <SidebarComponent />
-      </HeaderIconContainer>
-      <HeaderInputContainer>
-        <InputComponent />
-      </HeaderInputContainer>
-      <HeaderRightIconsContainer>
-        <HeaderRightComponent />
-      </HeaderRightIconsContainer>
-    </HeaderContainer>
-  </>
-))
+const HeaderComponent = memo(() => {
+  const theme = useContext(ThemeContext)
+
+  const { goBack } = useHistory()
+  const { location } = window
+
+  const isHomePage = useMemo(() => location.pathname === '/', [location])
+
+  const handlebackIconClick = useCallback(() => {
+    goBack()
+  }, [])
+
+  return (
+    <>
+      <HeaderContainer>
+        <HeaderIconContainer>
+          {isHomePage ? (
+            <SidebarComponent />
+          ) : (
+            <Icon
+              onClick={handlebackIconClick}
+              icon={faArrowLeft}
+              color={theme.color.primary}
+            />
+          )}
+        </HeaderIconContainer>
+        <HeaderInputContainer>
+          <InputComponent />
+        </HeaderInputContainer>
+        <HeaderRightIconsContainer>
+          <HeaderRightComponent />
+        </HeaderRightIconsContainer>
+      </HeaderContainer>
+    </>
+  )
+})
 
 export default HeaderComponent
