330479f92c44da4f05df54ff1b201799f33be8cd
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-11-14 09:42:31 +0500

    !feat(CT-29/contactFields): marker position changed on autocomplete

--- src/pages/contact/components/inputFields/useContactForm/index.ts
+++ src/pages/contact/components/inputFields/useContactForm/index.ts
@@ -1,9 +1,9 @@
 import { ChangeEvent, useCallback } from 'react'
 import { usePlacesWidget } from 'react-google-autocomplete'
 import { useDispatch, useSelector } from 'react-redux'
-import { IGeoAddressType} from '../../../../../types/geoLocation/index';
+import { IGeoAddressType } from '../../../../../types/geoLocation/index'
 import { GOOGLE_MAP_API_KEY } from '../../../../../config/constants'
-import { setGeoAddressState } from '../../../../../redux/features/geoLocatonSlice'
+import { setGeoAddressState, setGeoLocationState } from '../../../../../redux/features/geoLocatonSlice'
 import { RootState } from '../../../../../redux/store'
 
 const useContactForm = () => {
@@ -13,16 +13,30 @@ const useContactForm = () => {
   )
   const handlePlaceSelected = useCallback(
     // eslint-disable-next-line no-undef
-    (places: google.maps.places.PlaceResult) => console.info(places),
+    (places: google.maps.places.PlaceResult) => {
+      console.log(places.geometry?.location)
+      const {geometry} = places;
+      if (geometry) {
+        // eslint-disable-next-line no-undef
+        const _obj : google.maps.LatLngLiteral = {
+          lat: geometry.location!.lat(),
+          lng: geometry.location!.lng()
+        }
+        dispatch(setGeoLocationState(_obj))
+      }
+    },
     [address?.location]
   )
 
-  const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault()
-    const {name, value} = e.target;
-    const _key : keyof IGeoAddressType = name as keyof IGeoAddressType
-    dispatch(setGeoAddressState({name: _key, value}))
-  }, [address])
+  const handleChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      e.preventDefault()
+      const { name, value } = e.target
+      const _key: keyof IGeoAddressType = name as keyof IGeoAddressType
+      dispatch(setGeoAddressState({ name: _key, value }))
+    },
+    [address]
+  )
 
   const { ref: autoCompleteRef } = usePlacesWidget({
     apiKey: GOOGLE_MAP_API_KEY,
