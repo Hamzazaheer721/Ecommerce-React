5e60a296f0967dbf0b84dc9a043c08995693dfc9
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-11-17 11:20:52 +0500

    !fix(CT-29/contactFields): refactored the hook

--- src/pages/contact/components/inputFields/components/locationFields/useLocationForm/helper.ts
+++ src/pages/contact/components/inputFields/components/locationFields/useLocationForm/helper.ts
@@ -1,15 +1,4 @@
-import { IGeoAddressType } from '../../../../../../../types/geoLocation/index'
 import { IContactStateType } from './types'
-
-export const IContactInitialState: Partial<IGeoAddressType> = {
-  area: '',
-  city: '',
-  country: '',
-  location: '',
-  postalCode: '',
-  state: '',
-  streetAddress: ''
-}
 
 export const initialContactState: IContactStateType = {
   is_online: false
