07e9f333f1d100888c9ecc26503b52bfa5d7fdd6
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-11-17 13:00:05 +0500

    !fix(CT-29/contactFields): generic checkbox has been created

---
+++ src/components/checkbox/index.styled.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import { Checkbox } from 'antd';
+
+export const CustomCheckBox = styled(Checkbox)`
+`
+
+export const CheckboxContainer = styled.div`
+`
---
+++ src/components/checkbox/index.tsx
@@ -0,0 +1,16 @@
+import { CheckboxChangeEvent } from 'antd/lib/checkbox'
+import { FC, memo } from 'react'
+import { CheckboxContainer, CustomCheckBox } from './index.styled'
+
+interface ICheckboxInterface {
+  // eslint-disable-next-line no-unused-vars
+  handleChange: (e: CheckboxChangeEvent) => void
+  label?: string
+}
+const Checkbox: FC<ICheckboxInterface> = memo(({ handleChange, label }) => (
+  <CheckboxContainer>
+    <CustomCheckBox onChange={handleChange}>{label}</CustomCheckBox>
+  </CheckboxContainer>
+))
+
+export default Checkbox
