329a12a5543634bf5c42471281effa41f362c519
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-11-21 19:28:03 +0500

    !hotfix: handleSubmit in progress

--- src/pages/contact/useContactField/index.ts
+++ src/pages/contact/useContactField/index.ts
@@ -1,3 +1,4 @@
+import produce from 'immer'
 import { MouseEvent, useCallback } from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../../redux/store'
@@ -6,6 +7,7 @@ const useContactFields = () => {
   const locationFieldsState = useSelector(
     (state: RootState) => state.currentAddressLocation
   )
+  const {is_online} = locationFieldsState;
 
   const contactFieldsState = useSelector(
     (state: RootState) => state.contactFields
@@ -14,9 +16,18 @@ const useContactFields = () => {
   const handleSubmit = useCallback((e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
     e.stopPropagation()
+
+    const _loc: typeof locationFieldsState = produce(locationFieldsState, (draft) => draft)
+    const _newLoc = {..._loc.address}
+    const _loc_data = {..._newLoc, is_online: _loc.is_online}
+    const _cont_data: typeof contactFieldsState = produce(contactFieldsState, (draft) => draft)
+    const _updatedData = {..._loc, ..._cont_data}
+
+    console.info('My data is ', _loc_data)
+    console.info('updated Data: ', _updatedData)
   }, [])
 
-  return { handleSubmit, locationFieldsState, contactFieldsState }
+  return { handleSubmit, is_online, locationFieldsState, contactFieldsState }
 }
 
 export default useContactFields
