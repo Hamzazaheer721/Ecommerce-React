b46acbb13cf821252dea667a079c68342167c3cb
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-11-21 09:21:52 +0500

    !hotfix: cleaned input fields component

--- src/pages/contact/components/inputFields/index.tsx
+++ src/pages/contact/components/inputFields/index.tsx
@@ -1,9 +1,7 @@
 import { memo } from 'react'
-// import { useLayoutEffect } from 'react'
 import { FieldsSectionContainer } from './index.styled'
 import ContactFields from './components/contactFields'
 import LocationFields from './components/locationFields'
-// import { loadMapApi } from '../../../../general/helper'
 
 const LocationInputFields = memo(() => (
   <>
