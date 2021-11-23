1f05b22badf61504eda628b125ceea353514f7ac
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-11-23 18:53:14 +0500

    !feat(CT-36/storeSettings): styling is being done

---
+++ src/pages/storeSettings/components/panelCard/index.styled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+interface IPanelCardContainerProps {
+  $grayColor?: boolean
+}
+
+export const PanelCardContainer = styled.div<IPanelCardContainerProps>`
+  display: flex;
+  width: 100%;
+  height: auto;
+  padding: 10px;
+  background-color: ${({$grayColor}) => ($grayColor ? '#F8F8F8' : '#FFFFFF')};
+  padding: 10px 30px;
+`
--- src/pages/storeSettings/components/panelCard/index.tsx
+++ src/pages/storeSettings/components/panelCard/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { IconProp } from '@fortawesome/fontawesome-svg-core'
 import { FC, memo } from 'react'
+import { PanelCardContainer } from './index.styled'
 
 interface IPanelCardProps {
   title: string
@@ -11,12 +12,12 @@ interface IPanelCardProps {
   switchButton?: boolean
 }
 
-const PanelCard: FC<IPanelCardProps> = memo(({ title }) => {
+const PanelCard: FC<IPanelCardProps> = memo(({ title, grayColor }) => {
   console.info('hey')
   return (
-    <>
+    <PanelCardContainer $grayColor={!!grayColor}>
       <div>{title}</div>
-    </>
+    </PanelCardContainer>
   )
 })
 
